a923532920208f22402e8790a9f92fe0a799b8f3
Micheál Looney <hello@micheallooney.com>
2015-12-31 09:34:59 +0000

    added some features but am now getting an import error. Saving work to rollback to previous version

--- redditbot.py
+++ redditbot.py
@@ -2,18 +2,19 @@ import praw
 import time
 
 
-
 r = praw.Reddit(user_agent = "replybot for comments from users I suspect are from Ireland")
 r.login()
 print("logging in...")
 
-comment_cache = []
-words_to_match = ["arse", "feck", "shite"]
+comment_cache = {}
+words_to_match = [" arse ", " feck ", " shite ", " cop on " , " culchie ", "eejit ", \
+                  " gaff ", " gammy " , " jackeen ", " langer " , " manky ", " naggin ", \
+                  " skanger " , " gobshite " ]
 
 
-def runBot():
+def runBot(mode):
     print("getting subreddit...")
-    subreddit = r.get_subreddit("test")
+    subreddit = r.get_subreddit("ireland")
     print("getting comments...")
     comments = subreddit.get_comments(limit=1000)
     print("comments retrieved, begin comparison...")
@@ -23,13 +24,34 @@ def runBot():
         isMatch = any(string in comment_text for string in words_to_match)
         if comment.id not in comment_cache and isMatch:
             print("comment found, id = " + str(comment.id))
-            comment.reply("Through sophisticated lexical analysis I have determined that you are from Ireland. Am I correct?")
-            comment_cache.append(comment.id)
-            print("comment replied to and cached")
+            comment_cache.update({comment.id:comment.body})
+            print("comment cached")
+            if mode == live:
+                comment.reply("Through sophisticated lexical analysis I have determined \
+                    that you are from Ireland. Am I correct?")
+                print("comment replied to")
+            
 
-while True:
-    runBot()
-    time.sleep(10)            
+    for commentID,commentText in comment_cache.items():
+        print("comment id: ")
+        print(commentID)
+        print("comment text: ")
+        print(commentText)
+        print("========================================================")       
+
+def userMenu():
+    print("Enter a number to specify which mode to operate in")
+    print("1 - Test mode - Any matches found are entered into a cache and printed \
+          the console once all comments have been analysed")
+    print("2 - 'Live mode' - Comments are replied to in real time by the bot. \
+          Be careful, this can annoy people")
+    choice = input("Enter number: ")
+    if choice == 1:
+        runBot("test")
+    elif choice == 2:
+        runBot("live")
+
+userMenu()
 
 
 
