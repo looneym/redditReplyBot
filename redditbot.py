1160459d7a858cab2915046da1c643c12a829935
Micheál Looney <hello@micheallooney.com>
2015-12-31 12:43:38 +0000

    better formatted reply string, other misc improvements and bug fixes

--- redditbot.py
+++ redditbot.py
@@ -6,16 +6,27 @@ r = praw.Reddit(user_agent = "replybot for comments from users I suspect are fro
 r.login()
 print("logging in...")
 
-comment_cache = {}
+content_tracker = {}
+actioned_comments = []
 words_to_match = [" arse ", " feck ", " shite ", " cop on " , " culchie ", " eejit ", \
                   " gaff ", " gammy " , " jackeen ", " langer " , " manky ", " naggin ", \
                   " skanger " , " gobshite " ]
+reply_string = "Through sophisticated lexical analysis I have determined that you are from Ireland. Am I correct? \n \
+\n \
+\n \
+----- \
+\n \
+\n \
+^I'm ^a ^bot ^that ^looks ^for ^idiomatically ^Irish ^expressions ^in ^comments ^and ^then ^pokes ^the ^people ^who ^wrote ^them ^to ^see ^if ^my ^hunch ^checks ^out. \
+\n \
+\n \
+^[source](https://github.com/looneym/redditReplyBot)"                 
 
 
 def runBot(userMode):
     print("Bot running. Mode = " + userMode)
     print("getting subreddit...")
-    subreddit = r.get_subreddit("test")
+    subreddit = r.get_subreddit("all")
     print("getting comments...")
     comments = subreddit.get_comments(limit=1000)
     print("comments retrieved, begin comparison...")
@@ -23,16 +34,25 @@ def runBot(userMode):
     for comment in comments:
         comment_text = comment.body.lower()
         isMatch = any(string in comment_text for string in words_to_match)
-        if comment.id not in comment_cache and isMatch:
+
+        if comment.id in actioned_comments:
+            print("bot has re-encountered a previously-actioned comment. ID: " + str(comment.id))
+
+        if comment.id not in actioned_comments and isMatch:
             print("comment found, id = " + str(comment.id))
-            comment_cache.update({comment.id:comment.body})
+            content_tracker.update({comment.id:comment.body})
+            actioned_comments.append(comment.id)
             print("comment cached")
             if userMode == "live" or userMode =="indefinite":
-                comment.reply("Through sophisticated lexical analysis I have determined \
-                    that you are from Ireland. Am I correct?")
-                print("comment replied to")
+                comment.reply(reply_string)
+                # comment.reply("Through sophisticated lexical analysis I have determined \
+                #     that you are from Ireland. Am I correct?")
+                print("comment replied to") 
 
-    for commentID,commentText in comment_cache.items():
+        
+
+
+    for commentID,commentText in content_tracker.items():
         print("comment id: ")
         print(commentID)
         print("comment text: ")
